bb144f981a6929bf6d9d7eacadb0e9a2b0493a91
CHABERT Audric <achabert@polytech.unice.fr>
2014-02-04 12:43:58 +0100

    PSIQJM-13 ajout d'exceptions et traitement

---
+++ updateclient/updateClient/BadParameterException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace updateClient 
+{
+    class BadParameterException : Exception
+    {
+        private string errorMessage;
+
+        public BadParameterException()
+        {
+            errorMessage = "empty error message";
+        }
+        public BadParameterException(string m)
+        {
+            errorMessage = m;
+        }
+        public string getErrorMessage()
+        {
+            return errorMessage;
+        }
+    }
+}
